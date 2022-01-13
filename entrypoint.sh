99c7b071901c091df7f3e686ecf9b899712f081f
zz <78016956+abc-zz@users.noreply.github.com>
2022-01-13 08:53:57 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-ls -l
+
+cd /root 
+wget http://ftp.us.debian.org/debian/pool/non-free/liba/libapache-mod-fastcgi/libapache2-mod-fastcgi_2.4.7~0910052141-1.1+deb8u1_amd64.deb 
+dpkg -i libapache2-mod-fastcgi_2.4.7~0910052141-1.1+deb8u1_amd64.deb 
+
+sed -i '$a ServerName 172.0.0.2:80' /etc/apache2/apache2.conf 
+service apache2 restart 
+
+a2enmod actions fastcgi 
+a2enmod rewrite 
+touch /usr/lib/cgi-bin/php7-fcgi
+chown -R www-data:www-data /usr/lib/cgi-bin
+
+
+
 service apache2 start
